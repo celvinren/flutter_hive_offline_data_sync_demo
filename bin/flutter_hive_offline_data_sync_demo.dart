b6b5e9c0845825738581998588f5af0d3879e287
Calvin Ren <calvin.qut@gmail.com>
2023-11-11 16:32:49 +1000

    init commit

---
+++ bin/flutter_hive_offline_data_sync_demo.dart
@@ -0,0 +1,5 @@
+import 'package:flutter_hive_offline_data_sync_demo/flutter_hive_offline_data_sync_demo.dart' as flutter_hive_offline_data_sync_demo;
+
+void main(List<String> arguments) {
+  print('Hello world: ${flutter_hive_offline_data_sync_demo.calculate()}!');
+}
---
+++ test/flutter_hive_offline_data_sync_demo_test.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_hive_offline_data_sync_demo/flutter_hive_offline_data_sync_demo.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
